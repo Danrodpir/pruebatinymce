7d26ecc4487feb3cb6a345f9d5543097a5b827f3
Danrodpir <danny_131@hotmail.com>
2021-04-09 13:08:11 +0100

    Eliminadas la menubar y toolbar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 tinymce.init({
     selector: '#myEditor',
-    height: 500,
-    plugins: [
-        'advlist autolink lists link image charmap print preview anchor',
-        'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table paste code help wordcount'
-    ],
-    toolbar: 'undo redo | formatselect | ' +
-        'bold | italic | backcolor | alignleft aligncenter ' +
-        'alignright alignjustify | bullist numlist outdent indent | ' +
-        'removeformat | help wordcount',
-    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+    height: 1000,
+    width: 5000,
+    plugins: "fontselect link autoresize code quickbars image lists media table hr",
+    toolbar: false,
+    menubar: false,
+    placeholder: "This is your story",
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+    quickbars_selection_toolbar: "bold italic link | alignleft aligncenter alignright | blockquote fontselect",
+    quickbars_insert_toolbar: "h1 h2 h3 code image media table hr",
 });
